17f9f3c958e74aeb19603a9f1183e73cfb885a6a
nathan <nathan.guardado@yahoo.com>
2023-11-29 20:36:07 -0500

    Finished

---
+++ answers/exercise18.sql
@@ -0,0 +1,2 @@
+ALTER TABLE Students ADD StudentID int;
+UPDATE Students SET City = 'Edinburgh', Country = 'Scotland' WHERE StudentID = 35;
---
+++ answers/exercise8.sql
@@ -0,0 +1,2 @@
+CREATE TABLE Students (StudentName varchar(255), Address varchar(255), City varchar(255), PostalCode varchar(255), Country varchar(255));
+INSERT INTO Students VALUES ('Jane Doe', '57 Union St', 'Glasgow', 'G13RB', 'Scotland');
